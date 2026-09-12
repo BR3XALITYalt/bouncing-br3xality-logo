180693d14aa111d80cb105857d6df0fe87d3ec62
BR3XALITY the BR3Xer <boostedreality@br3xality.org>
2026-09-12 11:44:39 -0700

    Adjust DVD logo speed and direction

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-let speed = 0.5;
+let speed = 1;
 let scale = 0.10;
 let canvas;
 let ctx;
@@ -7,8 +7,8 @@ let logoColor = '#ffffff';
 let dvd = {
     x: 200,
     y: 300,
-    xspeed: 10,
-    yspeed: 10,
+    xspeed: 5,
+    yspeed: 4.9,
     img: new Image()
 };
 
